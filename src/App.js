49796b2185de3bcb15c0d2fad7189a33d2b1960c
unknown <cogjava1780@iiht.tech>
2022-07-06 23:20:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import OrderForm from './orderform';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
+      <OrderForm name='Poonam'></OrderForm>
+      <OrderForm name='Jain'></OrderForm>
     </div>
   );
 }
---
+++ src/orderform.js
@@ -0,0 +1,10 @@
+function OrderForm(props) {
+    return (
+        <div>
+            <h1>order form</h1>
+            <input value={props.name}></input>
+        </div>
+    )
+}
+
+export default OrderForm;
