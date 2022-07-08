e8db7e240431d9b50f079316eea990717d6aeed1
Your Name <you@example.com>
2022-07-08 15:12:37 +0530

    fetching data from url

---
+++ src/AlbumList.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import './AlbumList.css';
+
+
+function AlbumList() {
+
+    let [photos,setPhotos] = useState([]);//in order to trigger the re-render, use-state
+    if(!photos.length){
+        getAlbums();
+    }
+    async function getAlbums() {
+        const photos = await (await fetch("https://jsonplaceholder.typicode.com/photos?_page=1")).json();//why twice
+        console.log(photos);
+        setPhotos(photos);
+    }
+    return(
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                    <td>Title</td>
+                    <td>URL</td>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {
+                        photos.map(function(photo){
+                        // console.log(photo);
+                        return<tr><td>{photo.title}</td>
+                                  <td>{photo.title}</td>
+                              </tr>
+                        })
+                    }
+                </tbody>
+            
+            </table>
+        </div>
+    )
+}
+
+export default AlbumList;
